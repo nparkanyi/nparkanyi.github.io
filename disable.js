b71eed1715636ba832284d794ef7a63230d77779
Nicholas Parkanyi <n.parkanyi@gmail.com>
2016-10-03 16:49:41 -0400

    Menu items activate when clicking anywhere in the box, not just the link.

--- disable.js
+++ disable.js
@@ -3,3 +3,11 @@ var elems = document.querySelectorAll("li.dropdown a[href='projects.html']");
 for (var i = 0; i < elems.length; i++){
     elems[i].href = "#";
 }
+
+elems = document.querySelectorAll("nav#menuBarNav li");
+for (var i = 0; i < elems.length; i++){
+    elems[i].addEventListener("click", function(event){
+        var url = event.target.querySelectorAll("li > a")[0].href;
+        window.location.href = url;
+    }, false)
+}
